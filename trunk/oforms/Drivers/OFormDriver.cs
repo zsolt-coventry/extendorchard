20875cfa0b78d184d536a1385afaaf83a36f1e00
mihai.marandici <mihai.marandici@edbb0343-3b67-36da-c52b-94d54a59f292>
2012-01-16 20:44:00 +0000

    1. OFormPart as an attachable content part 2. Created a new OForm content type that contains RoutePart, MenuPart, PublishLater

--- trunk/oforms/Drivers/OFormDriver.cs
+++ trunk/oforms/Drivers/OFormDriver.cs
@@ -1,15 +1,32 @@
-﻿using Orchard.ContentManagement.Drivers;
+﻿using System.Linq;
+using Orchard.ContentManagement.Drivers;
 using Orchard.ContentManagement;
 using oforms.Models;
+using oforms.Services;
+using Orchard.Localization.Services;
 
 namespace oforms.Drivers
 {
     public class OFormDriver : ContentPartDriver<OFormPart>
     {
+        private readonly ISerialService _serialService;
+        private readonly ICultureManager _cultureManager;
+
+        public OFormDriver(ISerialService serialService,
+            ICultureManager cultureManager)
+        {
+            this._serialService = serialService;
+            this._cultureManager = cultureManager;
+        }
+
         protected override DriverResult Display(OFormPart part, string displayType, dynamic shapeHelper)
         {
             return ContentShape("Parts_OForm_Display",
-                             () => shapeHelper.Parts_OForm_Display(ContentPart: part, ContentItem: part.ContentItem));
+                () => shapeHelper.Parts_OForm_Display(
+                    ContentPart: part, 
+                    ContentItem: part.ContentItem, 
+                    ValidSn: this._serialService.ValidateSerial(),
+                    Culture: GetCurrentSiteCulture()));
         }
 
         //GET
@@ -28,6 +45,17 @@ namespace oforms.Drivers
         {
             updater.TryUpdateModel(part, Prefix, null, null);
             return Editor(part, shapeHelper);
+        }
+
+        private string GetCurrentSiteCulture()
+        {
+            var siteCulture = _cultureManager.GetSiteCulture();
+            if (string.IsNullOrEmpty(siteCulture))
+            {
+                return null;
+            }
+
+            return siteCulture.Split('-').FirstOrDefault();
         }
     }
 }
